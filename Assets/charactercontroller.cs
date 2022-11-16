120cb906a3d973f24f12a6f825fef9e978817535
hakimemma <113105266+hakimemma@users.noreply.github.com>
2022-11-16 10:59:33 +0000

    camera accsess

--- Assets/charactercontroller.cs
+++ Assets/charactercontroller.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class charactercontroller : MonoBehaviour
-{
+{   float maxSpeed = 0.00001f;
+    float roatationSpeed = 2.0f;
+    float camRotation = 1.5f;
+    GameObject cam;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cam = GameObject.Find("Main Camera");
     }
-    float maxspeed = 0.00001f;
-    float roatation = 0.0f;
+    
     void Update()
     {
-       transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical"));
-       roatation = roatation + Input.GetAxis("Mouse X"); 
-       transform.rotation = Quaternion.Euler(new Vector3(0.0f, roatation, 0.0f));
+       transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxSpeed);
+       roatation = roatation + Input.GetAxis("Mouse X"); * rotationSpeed;
+       transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
+       camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed; 
+       
     }
 }
